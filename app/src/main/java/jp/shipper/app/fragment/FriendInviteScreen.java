97fa55d7e32ed12f2831750cb492c44821debd54
quynt0211 <quynt.ict@gmail.com>
2015-12-08 09:35:18 +0700

    create friendInviteScreen

---
+++ app/src/main/java/jp/shipper/app/fragment/FriendInviteScreen.java
@@ -0,0 +1,28 @@
+package jp.shipper.app.fragment;
+
+import android.os.Bundle;
+import android.view.View;
+
+import jp.shipper.app.R;
+
+/**
+ * Created by QuyTrongNguyen on 08/12/2015.
+ */
+public class FriendInviteScreen extends BaseFragment {
+    @Override
+    public int getLayout() {
+        return R.layout.friend_invite_screen;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        showHeader();
+        setHeaderTitle("Mời Kết Bạn");
+    }
+}
