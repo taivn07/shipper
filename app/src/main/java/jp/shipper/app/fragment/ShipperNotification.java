4916f76b560c84a822435685e72555a9eed5609b
quynt0211 <quynt.ict@gmail.com>
2015-12-10 13:40:00 +0700

    create ShipperNotification screen

--- app/src/main/java/jp/shipper/app/fragment/ShipperNotification.java
+++ app/src/main/java/jp/shipper/app/fragment/ShipperNotification.java
@@ -2,13 +2,22 @@ package jp.shipper.app.fragment;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 import jp.shipper.app.R;
+import jp.shipper.app.view.adapter.ShipperNotificationAdapter;
+import jp.shipper.app.view.item.ShipperNotificationItem;
 
 /**
  * Created by QuyTrongNguyen on 10/12/2015.
  */
 public class ShipperNotification extends BaseFragment {
+    private ListView mListView;
+    private ArrayList<ShipperNotificationItem> mData;
+    private ShipperNotificationAdapter mAdapter;
+
     @Override
     public int getLayout() {
         return R.layout.shipper_notification;
@@ -17,11 +26,19 @@ public class ShipperNotification extends BaseFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mData = new ArrayList<ShipperNotificationItem>();
+        for( int i = 0; i < 6; i ++ ){
+            ShipperNotificationItem item = new ShipperNotificationItem("content content content content content content content content content content " + i, i, "Date " + i );
+            mData.add(item);
+        }
     }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        mAdapter = new ShipperNotificationAdapter(getActivity(), mData );
+        mListView = ( ListView )view.findViewById(R.id.shipper_notification_listview);
+        mListView.setAdapter(mAdapter);
         showHeader();
         setHeaderTitle("Thông Báo");
     }
---
+++ app/src/main/java/jp/shipper/app/view/adapter/MerchantNotificationItem.java
@@ -0,0 +1,7 @@
+package jp.shipper.app.view.adapter;
+
+/**
+ * Created by QuyTrongNguyen on 10/12/2015.
+ */
+public class MerchantNotificationItem {
+}
