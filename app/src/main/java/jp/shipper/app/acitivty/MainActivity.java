cc2a25ad06e3d551519996c42caca3dc088ab25b
quynt0211 <quynt.ict@gmail.com>
2015-12-17 11:42:58 +0700

    Xử lý: ẩn bàn phìm khi click ở bên ngoài edittext

--- app/src/main/java/jp/shipper/app/acitivty/MainActivity.java
+++ app/src/main/java/jp/shipper/app/acitivty/MainActivity.java
@@ -1,7 +1,12 @@
 package jp.shipper.app.acitivty;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.Timer;
@@ -20,7 +25,7 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
-
+        setupUI(findViewById(R.id.main_activity_Rl));
         showFragment(new LoadingFragment());
 
         mTimerTask = new TimerTask() {
@@ -32,7 +37,6 @@ public class MainActivity extends BaseActivity {
 
         timer = new Timer();
         timer.schedule(mTimerTask, 2000);
-
     }
 
     public void showHeader() {
@@ -45,5 +49,37 @@ public class MainActivity extends BaseActivity {
 
     public void hideHeader() {
         findViewById(R.id.frame_header).setVisibility(View.GONE);
+    }
+
+    public static void hideSoftKeyboard(Activity activity) {
+        InputMethodManager inputMethodManager = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(activity.getCurrentFocus().getWindowToken(), 0);
+    }
+
+    public void setupUI(View view) {
+
+        //Set up touch listener for non-text box views to hide keyboard.
+        if (!(view instanceof EditText)) {
+
+            view.setOnTouchListener(new View.OnTouchListener() {
+
+                public boolean onTouch(View v, MotionEvent event) {
+                    hideSoftKeyboard(MainActivity.this);
+                    return false;
+                }
+
+            });
+        }
+
+        //If a layout container, iterate over children and seed recursion.
+        if (view instanceof ViewGroup) {
+
+            for (int i = 0; i < ((ViewGroup) view).getChildCount(); i++) {
+
+                View innerView = ((ViewGroup) view).getChildAt(i);
+
+                setupUI(innerView);
+            }
+        }
     }
 }
--- app/src/main/java/jp/shipper/app/acitivty/TabBarShipperActivity.java
+++ app/src/main/java/jp/shipper/app/acitivty/TabBarShipperActivity.java
@@ -1,22 +1,24 @@
 package jp.shipper.app.acitivty;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.MotionEvent;
 import android.view.View;
-import android.widget.BaseAdapter;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import jp.shipper.app.R;
-import jp.shipper.app.fragment.FooterFragment;
 import jp.shipper.app.fragment.FooterShipperFragment;
 import jp.shipper.app.fragment.FriendInviteScreen;
 import jp.shipper.app.fragment.GuideFragment;
-import jp.shipper.app.fragment.PersonalInfoFragment;
 import jp.shipper.app.fragment.ProfileEditScreen;
 import jp.shipper.app.fragment.ShareAppScreen;
 
@@ -41,7 +43,7 @@ public class TabBarShipperActivity extends BaseActivity implements View.OnClickL
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.navi_activity);
-
+        setupUI(findViewById(R.id.tab_bar_activity_Rl));
         findViewById(R.id.img_menu).setOnClickListener(this);
 
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -59,7 +61,7 @@ public class TabBarShipperActivity extends BaseActivity implements View.OnClickL
         showFragmentFooter(mFooter);
     }
 
-    public void showFragmentFooter(Fragment fragment){
+    public void showFragmentFooter(Fragment fragment) {
         FragmentManager fm = getFragmentManager();
         fm.beginTransaction().replace(R.id.frame_footer, fragment)
                 .commit();
@@ -97,26 +99,26 @@ public class TabBarShipperActivity extends BaseActivity implements View.OnClickL
         } else if (R.id.ll_personal_info == view.getId()) {
             toggleMenu();
             Toast.makeText(this, "Share", Toast.LENGTH_LONG).show();
-            if(MENU_PERSONAL_INFO != mCurrentMenu){
+            if (MENU_PERSONAL_INFO != mCurrentMenu) {
                 mCurrentMenu = MENU_PERSONAL_INFO;
                 showMenu(mCurrentMenu);
             }
         } else if (R.id.ll_guide == view.getId()) {
             toggleMenu();
-            if(MENU_GUIDE != mCurrentMenu){
+            if (MENU_GUIDE != mCurrentMenu) {
                 mCurrentMenu = MENU_GUIDE;
                 showMenu(mCurrentMenu);
             }
-        } else if( R.id.l6_share == view.getId()){
+        } else if (R.id.l6_share == view.getId()) {
             toggleMenu();
             Toast.makeText(this, "Share", Toast.LENGTH_LONG).show();
-            if( MENU_SHARE != mCurrentMenu ){
+            if (MENU_SHARE != mCurrentMenu) {
                 mCurrentMenu = MENU_SHARE;
                 showMenu(mCurrentMenu);
             }
-        }else if( R.id.l5_invite_friend == view.getId()){
+        } else if (R.id.l5_invite_friend == view.getId()) {
             toggleMenu();
-            if( MENU_INVITE != mCurrentMenu ){
+            if (MENU_INVITE != mCurrentMenu) {
                 mCurrentMenu = MENU_INVITE;
                 showMenu(mCurrentMenu);
             }
@@ -143,5 +145,37 @@ public class TabBarShipperActivity extends BaseActivity implements View.OnClickL
 
     public int getCurrentMenu() {
         return mCurrentMenu;
+    }
+
+    public static void hideSoftKeyboard(Activity activity) {
+        InputMethodManager inputMethodManager = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(activity.getCurrentFocus().getWindowToken(), 0);
+    }
+
+    public void setupUI(View view) {
+
+        //Set up touch listener for non-text box views to hide keyboard.
+        if (!(view instanceof EditText)) {
+
+            view.setOnTouchListener(new View.OnTouchListener() {
+
+                public boolean onTouch(View v, MotionEvent event) {
+                    hideSoftKeyboard(TabBarShipperActivity.this);
+                    return false;
+                }
+
+            });
+        }
+
+        //If a layout container, iterate over children and seed recursion.
+        if (view instanceof ViewGroup) {
+
+            for (int i = 0; i < ((ViewGroup) view).getChildCount(); i++) {
+
+                View innerView = ((ViewGroup) view).getChildAt(i);
+
+                setupUI(innerView);
+            }
+        }
     }
 }
