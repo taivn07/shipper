959b210b37537622833d53f6bee7e2277f2933b7
doducanh25 <doanh25@gmail.com>
2015-12-17 15:54:44 +0700

    update code fix map bug

--- app/src/main/java/jp/shipper/app/fragment/MapAddressFragment.java
+++ app/src/main/java/jp/shipper/app/fragment/MapAddressFragment.java
@@ -1,19 +1,25 @@
 package jp.shipper.app.fragment;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v4.app.FragmentActivity;
 import android.view.View;
 
-import jp.shipper.app.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapAddressFragment extends TabBaseFragment{
+import jp.shipper.app.R;
+
+/**
+ * Created by PaditechPC1 on 12/15/2015.
+ */
+public class MapAddressFragment extends TabBaseFragment implements OnMapReadyCallback {
     private GoogleMap googleMap;
     @Override
     public int getLayout() {
@@ -26,27 +32,31 @@ public class MapAddressFragment extends TabBaseFragment{
 
         setHeaderTitle(getString(R.string.tab_map));
         setCurrentMenu(0);
-        try {
-            // Loading map
-            initilizeMap();
-
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
     private void initilizeMap() {
-        if (googleMap == null) {
-            googleMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
-        }
-//		googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-        final LatLng Point = new LatLng(21 , 57);
-        Marker TP = googleMap.addMarker(new MarkerOptions().position(Point).title("choose address").icon(BitmapDescriptorFactory.fromResource(R.drawable.icon_pointer)));
+        SupportMapFragment mapFragment = (SupportMapFragment)((FragmentActivity)getActivity()).getSupportFragmentManager()
+                .findFragmentById(R.id.fragment_map);
+        mapFragment.getMapAsync(this);
+
     }
 
     @Override
     public void onResume() {
         super.onResume();
         initilizeMap();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng sydney = new LatLng(-34, 151);
+        googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+    public void onDestroyView() {
+        super.onDestroyView();
+        SupportMapFragment mapFragment = (SupportMapFragment)((FragmentActivity)getActivity()).getSupportFragmentManager()
+                .findFragmentById(R.id.fragment_map);
+        if (mapFragment!= null)
+            ((FragmentActivity)getActivity()).getSupportFragmentManager().beginTransaction().remove(mapFragment).commit();
     }
 }
